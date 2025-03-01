3d65860861eb283d6f792e456f4dcb33ff92bf75
JStrm <43290807+JStrm@users.noreply.github.com>
2025-03-01 23:21:27 +0100

    Try bigger matrix

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ mod inverse_matrix_finder;
 
 fn main() {
     let mut matrix = vec![
-        vec![RatNum::from_int(2), RatNum::from_int(0)],
-        vec![RatNum::from_int(0), RatNum::from_int(2)]
+        vec![RatNum::from_int(7), RatNum::from_int(2), RatNum::from_int(1)],
+        vec![RatNum::from_int(0), RatNum::from_int(3), RatNum::from_int(-1)],
+        vec![RatNum::from_int(-3), RatNum::from_int(4), RatNum::from_int(-2)]
     ];
 
     inverse_matrix_finder::invert(&mut matrix);
