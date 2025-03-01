905925cd0e904d0ecdcdd9f23d4a5cee304cc5ce
JStrm <43290807+JStrm@users.noreply.github.com>
2025-03-02 00:31:53 +0100

    Add console input

---
+++ src/console_matrix_input.rs
@@ -0,0 +1,63 @@
+use std::io;
+use crate::rational_number::RatNum;
+
+pub fn get() -> Vec<Vec<RatNum>> {
+    println!("Enter size of square matrix: ");
+    let n = read_number_from_input();
+
+    println!("Enter each integer element of matrix.");
+    println!("Separate number on a row by spaces");
+    println!("         and row by newlines.");
+
+    let matrix= read_matrix_from_input(n);
+
+    matrix
+}
+
+fn read_number_from_input() -> usize {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    input
+        .trim()
+        .parse()
+        .expect("Please enter a valid positive matrix size")
+}
+
+fn read_matrix_from_input(n: usize) -> Vec<Vec<RatNum>> {
+    let mut matrix: Vec<Vec<RatNum>> = Vec::with_capacity(n);
+
+    for _ in 0..n {
+        let mut row_input = String::new();
+        io::stdin()
+            .read_line(&mut row_input)
+            .expect("Failed to read line");
+
+        let int_row: Vec<isize> = row_input
+            .split_whitespace()
+            .map(|s| s.parse().expect("Invalid integer input"))
+            .collect();
+
+        if int_row.len() != n {
+            panic!(
+                "Invalid number of elements. Expected {}, got {}.",
+                n,
+                int_row.len()
+            );
+        }
+
+        let mut ratnum_row: Vec<RatNum> = Vec::with_capacity(n);
+
+        for integer in int_row.iter() {
+            ratnum_row.push(RatNum::from_int(*integer));
+        }
+
+
+
+        matrix.push(ratnum_row);
+    }
+
+    matrix
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate core;
+
 use crate::rational_number::RatNum;
 
 mod gaussian_elimination;
@@ -5,26 +7,30 @@ mod inverse_matrix_finder;
 mod matrix_manipulator;
 mod rational_number;
 mod pretty_matrix_printer;
+mod console_matrix_input;
 
 fn main() {
-    // TODO: Implement user input
+    // let mut matrix = console_matrix_input::get();
+
     let mut matrix = vec![
         vec![
-            RatNum::from_int(7),
-            RatNum::from_int(2),
             RatNum::from_int(1),
-        ],
-        vec![
-            RatNum::from_int(0),
+            RatNum::from_int(2),
             RatNum::from_int(3),
-            RatNum::from_int(-1),
         ],
         vec![
-            RatNum::from_int(-3),
             RatNum::from_int(4),
-            RatNum::from_int(-2),
+            RatNum::from_int(5),
+            RatNum::from_int(6),
+        ],
+        vec![
+            RatNum::from_int(7),
+            RatNum::from_int(8),
+            RatNum::from_int(9),
         ],
     ];
+
+    // TODO: debug this weird behaviour
 
     inverse_matrix_finder::invert(&mut matrix);
 
